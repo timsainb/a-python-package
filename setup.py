1c130ebd3b0d450be4e18644441df2576f6b7ceb
Tim Sainburg <timsainb@gmail.com>
2022-02-08 13:54:04 -0500

    update

--- setup.py
+++ setup.py
@@ -12,16 +12,7 @@ root_dir = pathlib.Path(__file__).parent.resolve()
 sys.path.insert(0, root_dir)
 
 # Get source directory.
-src_dir = root_dir / "src"
-
-# Read package info.
-about_file = src_dir / "foo" / "__about__.py"
-about = {}
-exec(about_file.read_text(), about)
-
-# Read long description from README file.
-readme_file = root_dir / "README.rst"
-long_description = readme_file.read_text(encoding="utf-8")
+src_dir = root_dir / "foo"
 
 # Find packages within source directory.
 packages = setuptools.find_packages(where=str(src_dir))
@@ -31,15 +22,14 @@ docs_requirements_file = root_dir / "docs" / "requirements.txt"
 docs_requirements = docs_requirements_file.read_text().splitlines()
 
 setuptools.setup(
-    name=about["__package__"],
-    version=about["__version__"],
-    description=about["__description__"],
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    url=about["__url__"],
-    author=about["__author__"],
-    author_email=about["__author_email__"],
-    license=about["__license__"],
+    name="foo",
+    version="0.0.9",
+    description="A Python package test2",
+    long_description="",
+    url="",
+    author="",
+    author_email="",
+    license="MIT",
     # https://pypi.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -56,17 +46,14 @@ setuptools.setup(
         "Topic :: Software Development :: Build Tools",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    packages=packages,
-    package_dir={
-        "": "src",  # Use relative path
-    },
+    packages=find_packages(),
     # https://python-packaging.readthedocs.io/en/latest/command-line-scripts.html#the-console-scripts-entry-point
     # https://click.palletsprojects.com/en/7.x/setuptools/
-    entry_points={"console_scripts": ["foo=foo.__main__:cli"]},
+    # entry_points={"console_scripts": ["foo=foo.__main__:cli"]},
     install_requires=["numpy>=1.20", "click"],
     extras_require={
-        "test": ["pytest", "coverage"],
-        "lint": ["pylama", "isort", "mypy"],
+        # "test": ["pytest", "coverage"],
+        # "lint": ["pylama", "isort", "mypy"],
         "docs": docs_requirements,
     },
     python_requires=">=3.7",
